6caa51246c58b8463912b6afe1209e73dc12fb39
DmalTzi <audyomg@gmail.com>
2024-03-26 17:22:05 +0700

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+function move(event){
+    let btnDOM = event.target
+    let ILikeToMoveItDOM = document.getElementsByClassName("ILikeToMoveIt")
+    ILikeToMoveItDOM[0].style.height = "30vh"
+    ILikeToMoveItDOM[0].style.width = "50vw"
+    let x = Math.floor(Math.random()*80)
+    let y = Math.floor(Math.random()*80)
+    btnDOM.style.top = `${y}%`
+    btnDOM.style.left = `${x}%`
+}
+
+async function ah(event){
+    let inputDOM = document.querySelector("input[name='SaySomeThing']")
+    let saySomeThing = document.getElementById("SaySomeThing")
+    saySomeThing.innerText = inputDOM.value
+    try{
+        let text = JSON.stringify({text:inputDOM.value})
+        await fetch("http://localhost:8000/api",{
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: text
+        })
+    }catch{
+        console.log(":(")
+    }
+}
---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+app = express()
+
+mongoose.connect("mongodb://localhost:27017",{dbName:"ClickMe"})
+
+const textSchema = new mongoose.Schema({
+    text: String
+})
+
+const Text = mongoose.model("text", textSchema)
+
+app.use(express.json())
+app.use(cors())
+
+app.post("/api", async(req,res)=>{
+    try {
+        await Text.create(req.body)
+        res.status(200).json("Good!")
+    } catch (error) {
+        res.status(500).json("Error!")
+    }
+})
+
+app.listen(8000,()=>{
+    console.log("server is running on port 8000")
+})
